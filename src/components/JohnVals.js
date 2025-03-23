a8c83101702f395b1651d016bceed1e56c387760
BenRzasa <rzasaben@gmail.com>
2025-03-23 14:43:00 -0600

    John and NAN value dictionaries fully added. Next is to finish up the CSV parsing into a map which then needs to be read properly in the table component.

---
+++ src/components/JohnVals.js
@@ -0,0 +1,385 @@
+// Dictionaries for John and NAN values
+export const johnValsDict = {
+  Rares: [
+    { name: "Ambrosine", baseValue: 1.0 },
+    { name: "Universallium", baseValue: 0.1 },
+    { name: "Neutrine", baseValue: 0.01 },
+    { name: "Torn Fabric", baseValue: 0.002 },
+    { name: "Singularity", baseValue: 0.001 },
+    { name: "Egg", baseValue: 0.02 },
+    { name: "Cindrasil", baseValue: 0.25 },
+    { name: "Zynulvinite", baseValue: 1.0 },
+    { name: "Element V", baseValue: 0.2 },
+    { name: "Neutrino", baseValue: 1.0 },
+    { name: "Malbrane", baseValue: 1.0 },
+    { name: "Dystranum", baseValue: 0.067 },
+    { name: "Ectokelyte", baseValue: 0.333 },
+    { name: "Havicron", baseValue: 0.1 },
+    { name: "Rhylazil", baseValue: 0.02 },
+    { name: "Ubriniale", baseValue: 0.01 },
+    { name: "Nyrvinoris", baseValue: 1.0 },
+    { name: "Unobtanium", baseValue: 1.0 }
+  ],
+
+  Uniques: [
+    { name: "Vicious Shard", baseValue: 0.004 },
+    { name: "Jalabono", baseValue: 1.0 },
+    { name: "Hollevite", baseValue: 1.0 },
+    { name: "Verglazium", baseValue: 2.0 },
+    { name: "Meteorite", baseValue: 2.0 },
+    { name: "Panolethrium", baseValue: 1.0 },
+    { name: "Astathian", baseValue: 0.5 },
+    { name: "Sunstone", baseValue: 5.0 },
+    { name: "Amber", baseValue: 2.0 },
+    { name: "Chalcedony", baseValue: 2.5 },
+    { name: "Onyx", baseValue: 2.5 }
+  ],
+
+  Compounds: [
+    { name: "Equilibrium", baseValue: 4.5 },
+    { name: "Quark Matter", baseValue: 2.5 },
+    { name: "Periglise", baseValue: 2.5 },
+    { name: "Isoronil", baseValue: 1 }
+  ],
+
+  SurfaceShallow: [
+    { name: "Stone", baseValue: 2000 },
+    { name: "Coal", baseValue: 150 },
+    { name: "Moonstone", baseValue: 4 },
+    { name: "Kyanite", baseValue: 6 },
+    { name: "Topaz", baseValue: 2 },
+    { name: "Opal", baseValue: 5 },
+    { name: "Aluminum", baseValue: 150 },
+    { name: "Copper", baseValue: 300 },
+    { name: "Iron", baseValue: 150 },
+    { name: "Sulfur", baseValue: 75 },
+    { name: "Silver", baseValue: 75 },
+    { name: "Zinc", baseValue: 75 },
+    { name: "Gold", baseValue: 18 },
+    { name: "Chlorophyte", baseValue: 9 },
+    { name: "Sapphire", baseValue: 10 }
+  ],
+
+  CavernsDusk: [
+    { name: "Ruby", baseValue: 11 },
+    { name: "Emerald", baseValue: 24 },
+    { name: "Peridot", baseValue: 6 },
+    { name: "Amethyst", baseValue: 4 },
+    { name: "Thallium", baseValue: 7 },
+    { name: "Tungsten", baseValue: 7 },
+    { name: "Diamond", baseValue: 10 },
+    { name: "Garnet", baseValue: 7 },
+    { name: "Platinum", baseValue: 9 },
+    { name: "Malachite", baseValue: 4.5 },
+    { name: "Lithium", baseValue: 8 },
+    { name: "Boron", baseValue: 75 }
+  ],
+
+  Volatile: [
+    { name: "Boomite", baseValue: 30 },
+    { name: "Titanium", baseValue: 8 },
+    { name: "Plutonium", baseValue: 10 },
+    { name: "Technetium", baseValue: 11 },
+    { name: "Uranium", baseValue: 14 },
+    { name: "Caesium", baseValue: 9 },
+    { name: "Osmium", baseValue: 10 },
+    { name: "Hematite", baseValue: 58.75 }
+  ],
+
+  MysticInbetween: [
+    { name: "Rose Quartz", baseValue: 8 },
+    { name: "Rainbonite", baseValue: 2.5 },
+    { name: "Barium", baseValue: 6.5 },
+    { name: "Chrysoprase", baseValue: 10 },
+    { name: "Soul Crystal", baseValue: 15 },
+    { name: "Cobalt", baseValue: 7 },
+    { name: "Lapis", baseValue: 7 },
+    { name: "Bismuth", baseValue: 9 },
+    { name: "Demonite", baseValue: 5.5 },
+    { name: "Mithril", baseValue: 3.5 }
+  ],
+
+  IgneousMantle: [
+    { name: "Vanadium", baseValue: 4 },
+    { name: "Dragonglass", baseValue: 45 },
+    { name: "Carnelian", baseValue: 22.5 },
+    { name: "Magmium", baseValue: 13 },
+    { name: "Firecrystal", baseValue: 12 },
+    { name: "Magnesium", baseValue: 11 },
+    { name: "Hellstone", baseValue: 24 },
+    { name: "Jasper", baseValue: 8.5 },
+    { name: "Mantle Fragment", baseValue: 14 },
+    { name: "Gargantium", baseValue: 12 }
+  ],
+
+  IrradiatedCaustic: [
+    { name: "Toxirock", baseValue: 500 },
+    { name: "Radium", baseValue: 10 },
+    { name: "Tellurium", baseValue: 8 },
+    { name: "Newtonium", baseValue: 31 },
+    { name: "Yunium", baseValue: 8 },
+    { name: "Thorium", baseValue: 14 },
+    { name: "Lead", baseValue: 92.5 },
+    { name: "Blastium", baseValue: 16 },
+    { name: "Coronium", baseValue: 40 },
+    { name: "Tritium", baseValue: 4.5 },
+    { name: "Polonium", baseValue: 4 }
+  ],
+
+  Mirage: [
+    { name: "Frightstone", baseValue: 40 },
+    { name: "Stellarite", baseValue: 4.5 },
+    { name: "Prismaline", baseValue: 4 },
+    { name: "Antimatter", baseValue: 12 },
+    { name: "Constellatium", baseValue: 5.5 },
+    { name: "Stratocrit", baseValue: 6 },
+    { name: "Dark Matter", baseValue: 10 }
+  ],
+
+  Gloom: [
+    { name: "Crimstone", baseValue: 70.5 },
+    { name: "Horrorstone", baseValue: 11 },
+    { name: "Strange Matter", baseValue: 6 },
+    { name: "Vermillium", baseValue: 9 },
+    { name: "Magnetite", baseValue: 4 },
+    { name: "Iridium", baseValue: 1000 },
+    { name: "Surrenderock", baseValue: 625 }
+  ],
+
+  Void: [
+    { name: "Vantaslate", baseValue: 450 },
+    { name: "Cometite", baseValue: 7 },
+    { name: "Adamantite", baseValue: 3 },
+    { name: "Eclipsium", baseValue: 6 },
+    { name: "Ebonakite", baseValue: 10 },
+    { name: "Palladium", baseValue: 6.5 },
+    { name: "Neptunium", baseValue: 5 },
+    { name: "Void Orb", baseValue: 7 },
+    { name: "Nilidust", baseValue: 6.5 },
+    { name: "Asthenocrit", baseValue: 6.5 }
+  ],
+
+  Grayscale: [
+    { name: "Gallium", baseValue: 60 },
+    { name: "Amnesite", baseValue: 18 },
+    { name: "Shadow Quartz", baseValue: 4 },
+    { name: "Glowstone", baseValue: 3.5 },
+    { name: "Graphene", baseValue: 6 },
+    { name: "Quicksilver", baseValue: 8 },
+    { name: "Gray Matter", baseValue: 5.5 }
+  ],
+
+  AchroWhitespace: [
+    { name: "Cadmium", baseValue: 12 },
+    { name: "Chromium", baseValue: 9 },
+    { name: "Null", baseValue: 8 },
+    { name: "Geometrium", baseValue: 3.5 },
+    { name: "Noise", baseValue: 4 },
+    { name: "Inversium", baseValue: 3.5 },
+    { name: "Navitalc", baseValue: 8 },
+    { name: "Myriroule ", baseValue: 5.5 },
+    { name: "Argibar", baseValue: 4.5 },
+    { name: "Perilium", baseValue: 2 }
+  ],
+
+  Frigid: [
+    { name: "Blue Ice", baseValue: 10 },
+    { name: "Frostarium", baseValue: 10 },
+    { name: "Anetrium", baseValue: 13.5 },
+    { name: "Polarveril", baseValue: 2 },
+    { name: "Cryonine", baseValue: 3.5 },
+    { name: "Soimabarium", baseValue: 8 }
+  ],
+
+  Marine: [
+    { name: "Nerolin", baseValue: 12.5 },
+    { name: "Tenebris", baseValue: 8 },
+    { name: "Yilantibris", baseValue: 6 },
+    { name: "Aquamarine", baseValue: 12 },
+    { name: "Hydrolyth", baseValue: 8 },
+    { name: "Naquadah", baseValue: 10 },
+    { name: "Pearl", baseValue: 6 },
+    { name: "Eidoliphyll", baseValue: 2 }
+  ],
+
+  Cosmic: [
+    { name: "Cosmic Glass", baseValue: 17.5 },
+    { name: "Cosmaline", baseValue: 10 },
+    { name: "Astratine", baseValue: 6 },
+    { name: "Starsteel", baseValue: 8 },
+    { name: "Stardust", baseValue: 6 },
+    { name: "Endotravine", baseValue: 7 }
+  ],
+
+  Molten: [
+    { name: "Nickel", baseValue: 48 },
+    { name: "Molten Iron", baseValue: 15.5 },
+    { name: "Corium", baseValue: 9 },
+    { name: "Cinnabar", baseValue: 16 },
+    { name: "Molten Iron", baseValue: 16 },
+    { name: "Pyroscorium", baseValue: 9 },
+    { name: "Samarium", baseValue: 16 }
+  ],
+
+  Serenity: [
+    { name: "Bedrock", baseValue: 1000 },
+    { name: "Jade", baseValue: 5 },
+    { name: "Redsteel", baseValue: 5 },
+    { name: "Celesteel", baseValue: 4 },
+    { name: "Cryoplasm", baseValue: 4 },
+    { name: "Andrium", baseValue: 8 },
+    { name: "Neutronium", baseValue: 8 },
+    { name: "Shadowspec", baseValue: 2 },
+    { name: "Spiralium", baseValue: 2.5 },
+    { name: "Frozen Nitrogen", baseValue: 11 },
+    { name: "Orichalican", baseValue: 8 },
+    { name: "Scorched Bedrock", baseValue: 1000 }
+  ],
+  
+  Plasma: [
+    { name: "Plasma", baseValue: 2000 },
+    { name: "Pyroplasm", baseValue: 5.5 },
+    { name: "Crystalline Plasma", baseValue: 18.5 },
+    { name: "Electrium", baseValue: 6 },
+    { name: "Infernilus", baseValue: 7.5 },
+    { name: "Convectine", baseValue: 10 },
+    { name: "Protonium", baseValue: 9 },
+    { name: "Galvanium", baseValue: 6 },
+    { name: "Ferozium", baseValue: 1000 }
+  ],
+
+  Quantum: [
+    { name: "Gluonium", baseValue: 2.5 },
+    { name: "Up Quark", baseValue: 20 },
+    { name: "Down Quark", baseValue: 24 },
+    { name: "Positron", baseValue: 30 },
+    { name: "Supermatter", baseValue: 13.5 },
+    { name: "Charm Quark", baseValue: 10 },
+    { name: "Top Quark", baseValue: 15 },
+    { name: "Bottom Quark", baseValue: 15 },
+    { name: "Strange Quark", baseValue: 10 },
+    { name: "Crystal Photon", baseValue: 10 },
+    { name: "Photalizine", baseValue: 7 }
+  ],
+
+  Stability: [
+    { name: "Universal Barrier", baseValue: 1000 },
+    { name: "Rifted Barrier", baseValue: 6 },
+    { name: "Fabric", baseValue: 7 },
+    { name: "Mavrikine", baseValue: 6 }
+  ],
+
+  Planck: [
+    { name: "String", baseValue: 16 },
+    { name: "Hadronil", baseValue: 8 },
+    { name: "Axion", baseValue: 13.5 },
+    { name: "Graviton", baseValue: 11 },
+    { name: "Mesonyte", baseValue: 9 },
+    { name: "Tachyon", baseValue: 6.5 },
+    { name: "Desmentinum", baseValue: 10 }
+  ],
+
+  UpperInstability: [
+    { name: "Ailmentin", baseValue: 750 },
+    { name: "Territane", baseValue: 15 },
+    { name: "Sinfurmium", baseValue: 10 },
+    { name: "Gyrivarium", baseValue: 8 },
+    { name: "Krazmite", baseValue: 7 },
+    { name: "Tendrock", baseValue: 1000 },
+    { name: "Dark Energy", baseValue: 8 },
+    { name: "Corrodoil", baseValue: 30.5 },
+    { name: "Antireal", baseValue: 5 },
+    { name: "Hyperium", baseValue: 9 },
+    { name: "Thermisine", baseValue: 14 },
+    { name: "Alkanite", baseValue: 1000 }
+  ],
+
+  LowerInstability: [
+    { name: "Grimstone", baseValue: 2000 },
+    { name: "Phosphyll", baseValue: 44.5 },
+    { name: "Vrimsten", baseValue: 36.5 },
+    { name: "Zilithorus", baseValue: 20 },
+    { name: "Rimrock", baseValue: 8 },
+    { name: "Korundum", baseValue: 50 },
+    { name: "Cytosol", baseValue: 750 },
+    { name: "Ichor", baseValue: 8.5 },
+    { name: "Centriale", baseValue: 11.5 },
+    { name: "Neuroplast", baseValue: 16.5 },
+    { name: "Trinasine", baseValue: 9 },
+    { name: "Adrenilar", baseValue: 7 },
+    { name: "Macusmite", baseValue: 22 }
+  ],
+
+  Murk: [
+    { name: "Leptonyte", baseValue: 15 },
+    { name: "Mezihyrium", baseValue: 9 },
+    { name: "Taynalum", baseValue: 10 },
+    { name: "Thulinyl", baseValue: 2 }
+  ],
+
+  EventHorizon: [ 
+    { name: "Solarium", baseValue: 29.5 },
+    { name: "Exotic Matter", baseValue: 41.5 },
+    { name: "Nebulinite", baseValue: 11.5 },
+    { name: "Arkivyll", baseValue: 12 },
+    { name: "Chronon", baseValue: 11 },
+    { name: "Aurorum", baseValue: 24.5 },
+    { name: "Korenil", baseValue: 12 },
+    { name: "Stellar Sediment", baseValue: 1000 },
+    { name: "Apiastrine", baseValue: 18 }
+  ],
+  
+  Abyss: [ 
+    { name: "Abyssal Sludge", baseValue: 16 },
+    { name: "Primordian", baseValue: 10.5 },
+    { name: "Cosmodium", baseValue: 4.5 },
+    { name: "Hexflame", baseValue: 10 },
+    { name: "Tetraquark", baseValue: 9.5 },
+    { name: "Mauraline", baseValue: 10 }
+  ],
+  
+  InnerHorizon: [ 
+    { name: "Nihinoris", baseValue: 13.5 },
+    { name: "Infrarian", baseValue: 4.5 },
+    { name: "Ultranium", baseValue: 7 },
+    { name: "Auricallium", baseValue: 9 },
+    { name: "Zeronian", baseValue: 4.5 },
+    { name: "Exodian", baseValue: 9 }
+  ],
+  
+  Quintessence: [ 
+    { name: "Aetherice", baseValue: 8 },
+    { name: "Starniferus", baseValue: 11 },
+    { name: "Ethanerite", baseValue: 11 },
+    { name: "Lumenyl", baseValue: 11 },
+    { name: "Impervium", baseValue: 19.5 },
+    { name: "Kryposilus", baseValue: 7 },
+    { name: "Vythusilyte", baseValue: 9 }
+  ],
+  
+  Interstice: [ 
+    { name: "Nethrastine", baseValue: 100 },
+    { name: "Formicite", baseValue: 8 },
+    { name: "Raw Energy", baseValue: 15 },
+    { name: "Obliviril", baseValue: 5.5 },
+    { name: "Enceladrum", baseValue: 9 }
+  ],
+  
+  Empyrean: [ 
+    { name: "Evrasalt", baseValue: 100 },
+    { name: "Qylicrist", baseValue: 5 },
+    { name: "Kafsium", baseValue: 16 },
+    { name: "Zetaslime", baseValue: 15.5 },
+    { name: "Ochistrite", baseValue: 19.5 },
+    { name: "Reithum", baseValue: 6 },
+    { name: "Vorazylith", baseValue: 7.5 },
+    { name: "Truth Quark", baseValue: 8.5 },
+    { name: "Wish Alloy", baseValue: 13 },
+    { name: "Phantasmorite", baseValue: 8.5 },
+    { name: "Profelis", baseValue: 11.5 },
+    { name: "Ogleum", baseValue: 7 },
+    { name: "Protireal", baseValue: 20 },
+    { name: "Xerutherum", baseValue: 7.5 },
+    { name: "Mesmirian", baseValue: 13 }
+  ],
+};
--- src/pages/ValueChart.js
+++ src/pages/ValueChart.js
@@ -9,117 +9,18 @@ import { CSVContext } from '../context/CSVContext';
 import '../styles/ValueChart.css';
 import { Link } from 'react-router-dom';
 import TableComponent from '../components/TableComponent';
+import { johnValsDict } from '../components/JohnVals';
+import { nanValsDict } from '../components/NANVals';
 
 function ValueChart() {
   const { csvData } = useContext(CSVContext);
   const [currentMode, setCurrentMode] = useState(1); // 1: NV, 2: UV, 3: SV
   const [isJohnValues, setIsJohnValues] = useState(true);
 
-  const layerNames = [
-      "SurfaceShallow", "CavernsDusk", "Volatile", "MysticInbetween", 
-      "IgneousMantle", "IrradiatedCaustic", "Mirage", "Gloom", "Void", "Grayscale", 
-      "AchroWhitespace", "Frigid", "Marine", "Cosmic", "Molten", "Serenity", 
-      "PlasmaField", "Quantum", "Stability", "Planck", "UpperInstability", 
-      "LowerInstability", "Murk", "EventHorizon", "Abyss", "InnerHorizon", 
-      "Quintessence", "Interstice", "Empyrean"
-  ]
-  // Rare ore table data
-  const rareOres = [
-    { name: "Ambrosine", baseValue: 1.0 },
-    { name: "Universallium", baseValue: 0.1 },
-    { name: "Neutrine", baseValue: 0.01 },
-    { name: "Torn Fabric", baseValue: 0.002 },
-    { name: "Singularity", baseValue: 0.001 },
-    { name: "Egg", baseValue: 0.02 },
-    { name: "Cindrasil", baseValue: 0.25 },
-    { name: "Zynulvinite", baseValue: 1.0 },
-    { name: "Element V", baseValue: 0.2 },
-    { name: "Neutrino", baseValue: 1.0 },
-    { name: "Malbrane", baseValue: 1.0 },
-    { name: "Dystranum", baseValue: 0.067 },
-    { name: "Ectokelyte", baseValue: 0.333 },
-    { name: "Havicron", baseValue: 0.1 },
-    { name: "Rhylazil", baseValue: 0.02 },
-    { name: "Ubriniale", baseValue: 0.01 },
-    { name: "Nyrvinoris", baseValue: 1.0 },
-    { name: "Unobtanium", baseValue: 1.0 },
-  ];
-
-  // Unique ore table data
-  const uniqueOres = [
-    { name: "Vicious Shard", baseValue: 0.004 },
-    { name: "Jalabono", baseValue: 1.0 },
-    { name: "Hollevite", baseValue: 1.0 },
-    { name: "Verglazium", baseValue: 2.0 },
-    { name: "Meteorite", baseValue: 2.0 },
-    { name: "Panolethrium", baseValue: 1.0 },
-    { name: "Astathian", baseValue: 0.5 },
-    { name: "Sunstone", baseValue: 5.0 },
-    { name: "Amber", baseValue: 2.0 },
-    { name: "Chalcedony", baseValue: 2.5 },
-    { name: "Onyx", baseValue: 2.5 },
-  ];
-
-  // Dictionaries for John and NAN values
-  const johnValsDict = {
-    Compounds: [
-      { name: "Equilibrium", baseValue: 4.5 },
-      { name: "Quark Matter", baseValue: 2.5 },
-      { name: "Periglise", baseValue: 2.5 },
-      { name: "Isoronil", baseValue: 1 },
-    ],
-
-    SurfaceShallow: [
-      { name: "Stone", baseValue: 2000 },
-      { name: "Coal",  baseValue: 1 },
-      { name: "Moonstone", baseValue: 1 },
-      { name: "Kyanite", baseValue: 1 },
-      { name: "Topaz", baseValue: 1 },
-      { name: "Opal", baseValue: 1 },
-      { name: "Aluminum", baseValue: 1 },
-      { name: "Copper", baseValue: 1 },
-      { name: "Iron", baseValue: 1 },
-      { name: "Sulfur", baseValue: 1 },
-      { name: "Silver", baseValue: 1 },
-      { name: "Zinc", baseValue: 1 },
-      { name: "Gold", baseValue: 1 },
-      { name: "Chlorophyte", baseValue: 1 },
-      { name: "Sapphire", baseValue: 1 }
-    ],
-    // Add other layers
-  };
-
-  const nanValsDict = {
-    Compounds: [
-      { name: "Equilibrium", baseValue: 2.5 },
-      { name: "Quark Matter", baseValue: 2 },
-      { name: "Periglise", baseValue: 2 },
-      { name: "Isoronil", baseValue: 1 },
-    ],
-
-    SurfaceShallow: [
-      { name: "Stone", baseValue: 2000 },
-      { name: "Coal",  baseValue: 1 },
-      { name: "Moonstone", baseValue: 1 },
-      { name: "Kyanite", baseValue: 1 },
-      { name: "Topaz", baseValue: 1 },
-      { name: "Opal", baseValue: 1 },
-      { name: "Aluminum", baseValue: 1 },
-      { name: "Copper", baseValue: 1 },
-      { name: "Iron", baseValue: 1 },
-      { name: "Sulfur", baseValue: 1 },
-      { name: "Silver", baseValue: 1 },
-      { name: "Zinc", baseValue: 1 },
-      { name: "Gold", baseValue: 1 },
-      { name: "Chlorophyte", baseValue: 1 },
-      { name: "Sapphire", baseValue: 1 }
-    ],
-    // Add other layers
-  };
-
-
   // Compounds data (fetched from the dictionary)
-  // Clean up so that it can loop through an array of layer names
+  // Clean up so that it can loop through an array of layer names and
+  // get the data from the John and NAN dicts
+
   const compounds = isJohnValues 
                   ? johnValsDict["Compounds"] 
                   : nanValsDict["Compounds"];
@@ -184,6 +85,7 @@ function ValueChart() {
           </ul>
         </nav>
       </header>
+
       {/* Display current mode*/}
       <h1>Current Values: {isJohnValues ? "John's Values" : "NAN's Values"}</h1>
       <h2>Current Mode: {currentMode === 1 ? "NV" : currentMode === 2 ? "UV" : "SV"}</h2>
@@ -197,7 +99,7 @@ function ValueChart() {
         <span>John Values</span>
         </button>
         <button
-          style={{ backgroundImage: 'linear-gradient(144deg, #ffd700, #ff3c42 50%, #ff8c42)' }}
+          style={{ backgroundImage: 'linear-gradient(144deg,rgb(155, 44, 214),rgb(205, 85, 238) 50%,rgb(224, 123, 244))' }}
           onClick={() => setIsJohnValues(false)}
         >
           <span>NAN Values</span>
@@ -229,7 +131,7 @@ function ValueChart() {
       <div className="tables-container">
         {/* Rares Table */}
         <TableComponent
-          data={rareOres}
+          // data={rareOres}
           title="Rares"
           currentMode={currentMode}
           csvData={csvData}
@@ -238,7 +140,7 @@ function ValueChart() {
 
         {/* Uniques Table */}
         <TableComponent
-          data={uniqueOres}
+          // data={uniqueOres}
           title="Uniques"
           currentMode={currentMode}
           csvData={csvData}
